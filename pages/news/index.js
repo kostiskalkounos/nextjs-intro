e545cf71d4c0a61c9ffb0dfcebce84f90d32f2a7
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-19 18:13:50 +0200

    link pages together

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,19 @@
 // domain-name.com/news
 
+import Link from "next/link";
+
 const NewsPage = () => {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/cool-beans">Cool beans</Link>
+        </li>
+        <li>Even better</li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
