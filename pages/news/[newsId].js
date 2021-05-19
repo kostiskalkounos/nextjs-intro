07f712bd41e25b8aa99cedbb7ec35c09fe5c0888
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-19 18:00:07 +0200

    extract dynamic URL parameter values

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,15 @@
 // domain-name.com/news/something-important
 
+import { useRouter } from "next/router";
+
 const DetailPage = () => {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
+  // we could send a request to the backend API
+  // to fetch the news item with newsId
+
   return <h1>The Detail Page</h1>;
 };
 
